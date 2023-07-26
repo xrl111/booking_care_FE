8d33c19e7a522ad675b9ebc73a62694a3975fb3b
xrl111 <93368873+xrl111@users.noreply.github.com>
2023-07-26 18:01:42 +0700

    Load động gender trong form manage Redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -7,19 +7,31 @@ const getAllUsers = (inputId) => {
     return axios.get(`/api/get-all-users?id=${inputId}`)
 }
 
-const createNewUserService = (data)=>{
+const createNewUserService = (data) => {
     return axios.post(`/api/create-new-user`, data)
 }
 
-const deleteUserService = (userId)=>{
-    return axios.delete('/api/delete-user',{
-        data:{
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
             id: userId
-        }})
+        }
+    })
 }
 
-const  editUserService =(inputData)=>{
+const editUserService = (inputData) => {
     return axios.put('/api/edit-user', inputData)
 }
-export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService, editUserService }
+
+const getAllCodeService = (inputType) => {
+    return axios.get(`/api/allcode?type=${inputType}`)
+}
+export {
+    handleLoginApi,
+    getAllUsers,
+    createNewUserService,
+    deleteUserService,
+    editUserService,
+    getAllCodeService
+}
 
